550de986af0e11b93040cd92a14d8f3baa133214
Yocyy <yocy0522@gmail.com>
2021-12-15 02:08:58 +0900

    first commit

---
+++ src/pages/Top.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { Layout } from '../components/Layout/Layout'
+import { fetchPopularData } from '../apis/index'
+
+export const Top = () => {
+    useEffect(() => {
+        fetchPopularData().then((res) => {
+            console.log('data',res);
+        });
+    },[])
+    return (
+        <Layout />
+    )
+}
