f2fdd51078cb02df886f5f0ded92a023bb413b96
Yocyy <yocy0522@gmail.com>
2021-12-17 22:49:56 +0900

    Layoutのscss追加

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from '../Header/Header'
-
+import Style from './Layout.module.scss'
 export const Layout = ({children}) => {
     return (
-        <div>
+        <div className={Style.wrapper}>
             <Header />
-            {children}
+            <div className={Style.main}>
+                {children}
+            </div>
         </div>
     )
 }
