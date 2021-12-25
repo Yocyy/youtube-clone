df6d03f373d2dc2d69d9a56b2f2326dbfab7daa6
Yocyy <yocy0522@gmail.com>
2021-12-25 19:27:59 +0900

    レイテンシの解消

--- src/components/SideList/SideList.js
+++ src/components/SideList/SideList.js
@@ -1,27 +1,11 @@
-import React, { useEffect, useContext } from 'react'
+import React, {  useContext } from 'react'
 import { Store } from '../../store/index'
-import { feachRelatedData } from '../../apis/index'
 import { SideListItem } from '../SideListItem/SideListItem'
 import Style from './SideList.module.scss'
 
 export const SideList = () => {
-    const { globalState, setGlobalState } = useContext(Store)
-    const setRelatedVideo = async (id) => {
-        await feachRelatedData(id).then((res) => {
-            const dataArray = res.data.items;
-            for (var i = 0; i < dataArray.length; i++) {
-              if (!dataArray[i].hasOwnProperty('snippet')) {
-                dataArray.splice(i, 1)
-              }
-            }
-            setGlobalState({type: 'SET_RELATED', payload: {related: res.data.items}})
-        })
-    }
-    useEffect(()=>{
-        if (globalState.selected)
-            setRelatedVideo(globalState.selected.id)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [globalState.selected])
+    const { globalState} = useContext(Store)
+
     return (
         <div className={Style.sidenav}>
             {
